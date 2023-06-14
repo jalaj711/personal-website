a471a834705055ed4172abdaab3fb328222652cd
Jalaj Kumar <96870071+jalaj711@users.noreply.github.com>
2023-06-14 15:57:30 +0530

    functionalise 3d carousel
    Signed-off-by: Jalaj Kumar <96870071+jalaj711@users.noreply.github.com>

--- src/app/carousel/page.tsx
+++ src/app/carousel/page.tsx
@@ -1,9 +1,89 @@
+'use client'
+
 import styles from "./page.module.css";
 import { Brush, NT } from "src/fonts";
 import primaryBg from "../../../public/images/2.webp";
 import secondaryBg from "../../../public/images/3.webp";
+import { useEffect, useRef, useState } from "react";
+
+const project_data = [
+  {
+    title: "project 1",
+    description:
+      "some description about project 1.some description about project 1.some description about project 1.some description about project 1.some description about project 1.some description about project 1.",
+  },
+  {
+    title: "project 1",
+    description:
+      "some description about project 1.some description about project 1.some description about project 1.some description about project 1.some description about project 1.some description about project 1.",
+  },
+  {
+    title: "project 1",
+    description:
+      "some description about project 1.some description about project 1.some description about project 1.some description about project 1.some description about project 1.some description about project 1.",
+  },
+  {
+    title: "project 1",
+    description:
+      "some description about project 1.some description about project 1.some description about project 1.some description about project 1.some description about project 1.some description about project 1.",
+  },
+  {
+    title: "project 1",
+    description:
+      "some description about project 1.some description about project 1.some description about project 1.some description about project 1.some description about project 1.some description about project 1.",
+  },
+  {
+    title: "project 1",
+    description:
+      "some description about project 1.some description about project 1.some description about project 1.some description about project 1.some description about project 1.some description about project 1.",
+  },
+];
+
+const CarouselCell = (props: {
+  title: string;
+  description: string;
+  rotation: number;
+  zTranslate: number | string;
+}) => {
+  return (
+    <div
+      className={styles.carousel_cell}
+      style={{
+        transform: `rotateY(${props.rotation}deg) translateZ(${props.zTranslate})`,
+      }}
+    >
+      <div className={styles.carousel_cell_wrapper}>
+        <div
+          className={styles.carousel_cell_picture}
+          style={{ backgroundImage: `url(${secondaryBg.src})` }}
+        ></div>
+        {/* <div className={styles.carousel_cell_text}>
+          <div className={styles.carousel_cell_header}>{props.title}</div>
+          <div className={styles.carousel_cell_description}>
+            {props.description}
+          </div>
+        </div> */}
+      </div>
+    </div>
+  );
+};
 
 export default function Home() {
+  const [currIndex, setCurrIndex] = useState(0);
+  const [zTranslate, setZTranslate] = useState("");
+  const [angleMultFactor, setAngleMultFactor] = useState(0);
+
+  useEffect(() => {
+    const fullWidth = 40;
+    const unit = "vw";
+
+    setZTranslate(
+      Math.round(
+        fullWidth  / 1.7 / Math.tan(Math.PI / project_data.length)
+      ).toString() + unit
+    );
+    setAngleMultFactor(Math.round(360 / project_data.length));
+  }, []);
   return (
     <main>
       <section className={styles.section}>
@@ -20,12 +100,15 @@ export default function Home() {
         </div>
         <div className={styles.carousel_scene}>
           <div className={styles.carousel}>
-            <div className={styles.carousel_cell}>1</div>
-            <div className={styles.carousel_cell}>2</div>
-            <div className={styles.carousel_cell}>3</div>
-            <div className={styles.carousel_cell}>4</div>
-            <div className={styles.carousel_cell}>5</div>
-            <div className={styles.carousel_cell}>6</div>
+            {project_data.map((elem, index) => (
+              <CarouselCell
+                title={elem.title}
+                description={elem.description}
+                rotation={index * angleMultFactor}
+                zTranslate={zTranslate}
+                key={elem.title + index}
+              />
+            ))}
           </div>
         </div>
       </section>
