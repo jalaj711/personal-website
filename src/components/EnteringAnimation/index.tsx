dce2032df20e8e0edd21db3676dd23c852e9e506
Jalaj Kumar <96870071+jalaj711@users.noreply.github.com>
2023-06-25 14:27:44 +0530

    hide entering animation component after animation
    Signed-off-by: Jalaj Kumar <96870071+jalaj711@users.noreply.github.com>

--- src/components/EnteringAnimation/index.tsx
+++ src/components/EnteringAnimation/index.tsx
@@ -1,9 +1,21 @@
+import { useEffect, useRef } from "react";
 import styles from "./index.module.css";
 import { NT } from "src/fonts";
 
 export default function EnteringAnimation() {
+
+  const ref = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    setTimeout(() => {
+      if(ref.current) {
+        ref.current.style.display = "none";
+      }
+    }, 4100)
+  }, [])
+
   return (
-    <div className={styles.container}>
+    <div className={styles.container} ref={ref}>
       <div className={[styles.text, NT.className].join(" ")}>
         {["", "h", "i"].map((elem, index) => (
           <span
